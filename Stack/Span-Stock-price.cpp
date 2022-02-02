2011bee97cd574ccdca3653accfb8e9aaa86aaa3
taneesha srivastava <73888467+taneeshasrivastava@users.noreply.github.com>
2022-02-02 16:33:51 +0530

    Stock span problem :
    task : The stock span problem is a financial problem where we have a series of n daily price quotes for a stock and we need to calculate the span of stocks price for all n days. 
    The span Si of the stocks price on a given day i is defined as the maximum number of consecutive days just before the given day, for which the price of the stock on the current day is less than or equal to its price on the given day.
    For example, if an array of 7 days prices is given as {100, 80, 60, 70, 60, 75, 85}, then the span values for corresponding 7 days are {1, 1, 1, 2, 1, 4, 6}.
    Explanation:
    Brute force:   TC: O(n^2)
     For every i check in left direction. check first highest day when price higher than current price .
    there we get the index : 
    then find the span : i- previous highest index ;
    
    Efficient way : TC: O(N)
    1. take a stack (day(indices)  , prices) ;
    2.pop if get higher value than top of stack ;
    3. base case : if current value is the highest , corner case no value left in stack greater than that then, make
     span = index+1
    4.stack==empty , span = idx+1;

--- Stack/Span-Stock-price.cpp
+++ Stack/Span-Stock-price.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+void stockSpan(int price[],int n,int span[]){
+    stack<int> s;
+    s.push(0);
+    span[0]=1;
 
+    for(int i=1;i<n-1;i++){
+        int currPrice=price[i];
+        while(!s.empty() and price[s.top()]<=currPrice){
+            s.pop();
+        }
+        if(!s.empty()){
+            int preHigh=s.top();
+            span[i]=i-preHigh;
+        }
+        else{
+            span[i]=i+1;
+        }
+        s.push(i);
+    }
+}
+int main(){
+   
+    int price[]={100,80,60,70,60,75,85};
+    int n= sizeof(price)/sizeof(int);
+    int span[10000]={0};
+    stockSpan(price,n,span);
+    for(int i=0;i<n;i++){
+        cout<<span[i]<<" ";
+    }
+
+    return 0;
+}
